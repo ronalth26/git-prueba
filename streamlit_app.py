209767738112228b22579af1c84e61161db45ba9
ronalth26 <117491977+ronalth26@users.noreply.github.com>
2023-07-09 01:52:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ nombre_variables = ['longitud_sepalo', 'ancho_sepalo', 'longitud_petalo']
 # Carga de datos desde la web: descarga el CSV y lo carga como DataFrame
 iris = pd.read_csv('https://github.com/ronalth26/git-prueba/blob/df87a3cd8bef7f3a91a136afed2b3c5467fa433c/lista-residuos.csv',sep=';',encoding='iso-8859-1'
                  )
-iris
+iris.head()
 
 
