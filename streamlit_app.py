29329af87ff4c5a1cc35d2aa0dbc8a15bb68eb9f
ronalth26 <117491977+ronalth26@users.noreply.github.com>
2023-07-09 02:06:47 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,8 @@ iris
 
 iris.head(3)
 
+iris = pd.read_csv('https://github.com/ronalth26/git-prueba/blob/df87a3cd8bef7f3a91a136afed2b3c5467fa433c/lista-residuos.csv',sep=',',encoding='iso-8859-1'
+                 )
+iris
+
 
