e61b472ff61f59fad0d72a1fe63190da00104d9e
ronalth26 <117491977+ronalth26@users.noreply.github.com>
2023-07-09 01:54:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ nombre_variables = ['longitud_sepalo', 'ancho_sepalo', 'longitud_petalo']
 # Carga de datos desde la web: descarga el CSV y lo carga como DataFrame
 iris = pd.read_csv('https://raw.githubusercontent.com/ayrna/tutorial-scikit-learn-IMC/master/data/iris.csv',sep=',',encoding='iso-8859-1'
                  )
-iris
+iris.head(5)
 
 
