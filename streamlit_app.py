eaca61351b2e441e73dd2e935aebed377ac3fe41
ronalth26 <117491977+ronalth26@users.noreply.github.com>
2023-07-09 02:03:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,12 +9,4 @@ iris
 
 
 iris.head(9)
-<style scoped> .dataframe tbody tr th:only-of-type { vertical-align: middle; }
-.dataframe tbody tr th {
-    vertical-align: top;
-}
 
-.dataframe thead th {
-    text-align: right;
-}
-</style>
