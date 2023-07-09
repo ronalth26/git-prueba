6ff0a43e2e41f2f49264a7e19a705c1845158c7d
ronalth26 <117491977+ronalth26@users.noreply.github.com>
2023-07-09 02:31:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-nombre_variables = ['longitud_sepalo', 'ancho_sepalo', 'longitud_petalo']
+nombre_variables = ['POB_URBANA', 'QRESIDUOS_DOM']
 # Carga de datos desde la web: descarga el CSV y lo carga como DataFrame
 iris = pd.read_csv('https://raw.githubusercontent.com/ayrna/tutorial-scikit-learn-IMC/master/data/iris.csv',sep=',',encoding='iso-8859-1'
                  )
@@ -16,7 +16,7 @@ iris3 = pd.read_csv('https://raw.githubusercontent.com/ronalth26/git-prueba/mast
                  )
 iris3.head(2)
 
-iris4 = pd.read_csv('https://raw.githubusercontent.com/ronalth26/git-prueba/3c2bc32f088cba5752937f6d3ad386c58581a150/corto3.csv',sep=',',encoding='iso-8859-1'
+iris4 = pd.read_csv('https://raw.githubusercontent.com/ronalth26/git-prueba/3c2bc32f088cba5752937f6d3ad386c58581a150/corto3.csv',sep=',',encoding='iso-8859-1',names = nombre_variables
                  )
 iris4
 
