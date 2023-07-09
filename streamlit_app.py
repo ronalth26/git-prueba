89f05bc92055f33cdaceb13e56f2044c9102061b
ronalth26 <117491977+ronalth26@users.noreply.github.com>
2023-07-09 01:43:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-nombre_variables = ['FECHA_CORTE']
+nombre_variables = ['N_SEC']
 # Carga de datos desde la web: descarga el CSV y lo carga como DataFrame
 iris = pd.read_csv('https://github.com/ronalth26/git-prueba/blob/ccfc74218dfbe9876af6da2e4fc4f4104b37c66d/lista-residuos.csv',
                    names = nombre_variables)
