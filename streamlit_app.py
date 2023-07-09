3c2bc32f088cba5752937f6d3ad386c58581a150
ronalth26 <117491977+ronalth26@users.noreply.github.com>
2023-07-09 02:20:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,3 +15,7 @@ iris2
 iris3 = pd.read_csv('https://github.com/ronalth26/git-prueba/blob/c08197d989bdbf1a2e60014d230aa0907aa75f26/corto2.csv',sep=',',encoding='iso-8859-1'
                  )
 iris3
+
+iris4 = pd.read_csv('https://github.com/ronalth26/git-prueba/blob/c08197d989bdbf1a2e60014d230aa0907aa75f26/corto3.csv',sep=',',encoding='iso-8859-1'
+                 )
+iris4
