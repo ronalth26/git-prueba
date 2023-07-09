f337166fc8017af2ef1b80d09560751a1d11a1e3
ronalth26 <117491977+ronalth26@users.noreply.github.com>
2023-07-09 01:44:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 nombre_variables = ['N_SEC']
 # Carga de datos desde la web: descarga el CSV y lo carga como DataFrame
-iris = pd.read_csv('https://github.com/ronalth26/git-prueba/blob/ccfc74218dfbe9876af6da2e4fc4f4104b37c66d/lista-residuos.csv',
+iris = pd.read_csv('https://github.com/ronalth26/git-prueba/blob/ccfc74218dfbe9876af6da2e4fc4f4104b37c66d/lista-residuos.csv',sep=';',encoding='iso-8859-1',
                    names = nombre_variables)
 iris
 
