cb0bba4f61c366dba33458a4c10bba4be55b10f9
ronalth26 <117491977+ronalth26@users.noreply.github.com>
2023-07-09 01:39:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,11 @@
-from collections import namedtuple
-import altair as alt
-import math
-import numpy as np
 import pandas as pd
-import streamlit as st
 
-@st.cache
-def load_data():
-  df=pd.read_csv('https://github.com/ronalth26/git-prueba/blob/ccfc74218dfbe9876af6da2e4fc4f4104b37c66d/lista-residuos.csv',sep=';',encoding='iso-8859-1')
-  return df
-
-df=load_data()
+nombre_variables = ['longitud_sepalo', 'ancho_sepalo', 'longitud_petalo', 'ancho_petalo', 'clase']
+# Carga de datos desde la web: descarga el CSV y lo carga como DataFrame
+iris = pd.read_csv('https://raw.githubusercontent.com/ayrna/tutorial-scikit-learn-IMC/master/data/iris.csv',
+                   names = nombre_variables)
+iris
 
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
 
 
