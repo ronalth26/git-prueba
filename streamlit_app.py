0815f9b189ad327c1b70f609322bcebe0ed5b56f
ronalth26 <117491977+ronalth26@users.noreply.github.com>
2023-07-10 16:50:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,17 +4,6 @@ import altair as alt
 import numpy as np
 
 
-
-st.pyplot(fig)
-
-
-titanic = sns.load_dataset("titanic")
-
-fig = plt.figure(figsize=(10, 4))
-sns.countplot(x="class", data=titanic)
-
-st.pyplot(fig)
-
 st.set_page_config(page_title="Datos Abiertos") # Nombre para configurar la pagina web
 st.header('Residuos municipalidades generados anualmente') #Va a ser el titulo de la pagina
 st.subheader('La GPC de residuos domiciliarios es un dato obtenido de los estudios de caracterización elaborados por las municipalidades provinciales y distritales y se refiere a la generación de residuos sólidos por persona-día.') #Subtitulo
