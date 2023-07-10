1c634fe27d47a981e5db5806d88a4154f2e0e7b6
ronalth26 <117491977+ronalth26@users.noreply.github.com>
2023-07-10 09:44:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,21 +2,6 @@ import pandas as pd
 
 nombre_variables = ['POB_URBANA', 'QRESIDUOS_DOM']
 # Carga de datos desde la web: descarga el CSV y lo carga como DataFrame
-iris = pd.read_csv('https://raw.githubusercontent.com/ayrna/tutorial-scikit-learn-IMC/master/data/iris.csv',sep=',',encoding='iso-8859-1'
-                 )
-iris
-
-
-iris2 = pd.read_csv('https://raw.githubusercontent.com/ronalth26/git-prueba/master/corto.csv',sep=';',encoding='iso-8859-1',names = nombre_variables
-                 )
-iris2
-
-
-iris3 = pd.read_csv('https://raw.githubusercontent.com/ronalth26/git-prueba/master/corto2.csv',sep=',',encoding='iso-8859-1'
-                 )
-iris3
-
-
 
 data = pd.read_csv('https://raw.githubusercontent.com/ronalth26/git-prueba/master/lista-residuos.csv',sep=';',encoding='iso-8859-1'
                  )
