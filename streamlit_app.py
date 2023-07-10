2b2cbac21593753f87ec176114e7f44fe355f775
ronalth26 <117491977+ronalth26@users.noreply.github.com>
2023-07-10 10:20:38 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,5 @@ st.subheader('El objetivo sería determinar si existe una relación lineal entre
 tabla  = pd.read_csv('https://raw.githubusercontent.com/ronalth26/git-prueba/master/lista-residuos.csv',sep=';',encoding='iso-8859-1', names=nombre_variables
                  )
 tabla 
-
-
-
 tabla.head() 
+tabla 
