ff56b3f6410c63bc243ede3e100210b73492b163
ronalth26 <117491977+ronalth26@users.noreply.github.com>
2023-07-10 09:56:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,10 +16,3 @@ data = pd.read_csv('https://raw.githubusercontent.com/ronalth26/git-prueba/maste
                  )
 data
 
-import matplotlib.pyplot as plt
-plt.figure(figsize=(10,10))
-plt.plot(data['POB_URBANA'],data['QRESIDUOS_DOM'],'.', color='green')
-plt.title('Generación de residuos domiciliarios para una población urbana')
-plt.xlabel('QRESIDUOS_DOM')
-plt.ylabel('POB_URBANA')
-plt.show()
