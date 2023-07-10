394ba0c6bce612f5a199be20ff5aa46d75da6d26
ronalth26 <117491977+ronalth26@users.noreply.github.com>
2023-07-10 10:04:14 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,6 @@
 import pandas as pd
 
-
-
-
-
 nombre_variables = ['POB_URBANA', 'QRESIDUOS_DOM']
-# Carga de datos desde la web: descarga el CSV y lo carga como DataFrame
 
 excel_file  = pd.read_csv('https://raw.githubusercontent.com/ronalth26/git-prueba/master/lista-residuos.csv',sep=';',encoding='iso-8859-1'
                  )
