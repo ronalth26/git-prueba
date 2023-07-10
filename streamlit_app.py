7d1cf6cef0b5135288b62fb1ac38000a3e0b6861
ronalth26 <117491977+ronalth26@users.noreply.github.com>
2023-07-10 10:06:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,7 @@ import streamlit as st
 import plotly.express as px
 #from PIL import Image
 
-st.set_page_config(page_title="Encuesta Oficial EPS") # Nombre para configurar la pagina web
-st.header('Resultados Encuestas Nacionales EPS Colombia 2022') #Va a ser el titulo de la pagina
-st.subheader('Cómo perciben los ciudadanos el servicio de las EPS en Colombia?') #Subtitulo
+
 
 nombre_variables = ['POB_URBANA', 'QRESIDUOS_DOM']
 
