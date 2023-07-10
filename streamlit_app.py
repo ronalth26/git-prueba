eef941793ebd2ed6aef22992026b90c0cafb5b7a
ronalth26 <117491977+ronalth26@users.noreply.github.com>
2023-07-10 10:24:57 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.subheader('La GPC de residuos domiciliarios es un dato obtenido de los estudi
 
 nombre_variables = ["POB_URBANA", "QRESIDUOS_DOM"]
 
-excel_file  = pd.read_csv('https://raw.githubusercontent.com/ronalth26/git-prueba/master/lista-residuos.csv',sep=';',encoding='iso-8859-1'
+excel_file  = pd.read_csv('https://raw.githubusercontent.com/ronalth26/git-prueba/master/lista-residuos.csv',sep=';',encoding='iso-8859-1',index_col=0, header =3
                  )
 excel_file 
 
