1bc9ef924eae9020f8a73dc9ae6cf9c1d73ef0a2
ronalth26 <117491977+ronalth26@users.noreply.github.com>
2023-07-10 10:57:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import streamlit as st
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 
 
 #from PIL import Image
@@ -10,21 +12,18 @@ st.subheader('La GPC de residuos domiciliarios es un dato obtenido de los estudi
 
 nombre_variables = ["POB_URBANA", "QRESIDUOS_DOM"]
 
-excel_file  = pd.read_csv('https://raw.githubusercontent.com/ronalth26/git-prueba/master/lista-residuos.csv',sep=';',encoding='iso-8859-1'
+df  = pd.read_csv('https://raw.githubusercontent.com/ronalth26/git-prueba/master/lista-residuos.csv',sep=';',encoding='iso-8859-1'
                  )
-excel_file 
+df 
 
 
 st.subheader('El objetivo sería determinar si existe una relación lineal entre dos variables y predecir la generación de residuos domiciliarios para una población determinada.') #Subtitulo
 
-df  = pd.read_csv('https://raw.githubusercontent.com/ronalth26/git-prueba/master/lista-residuos.csv',sep=';',encoding='iso-8859-1',  usecols=["POB_URBANA", "QRESIDUOS_DOM"]                       
+data  = pd.read_csv('https://raw.githubusercontent.com/ronalth26/git-prueba/master/lista-residuos.csv',sep=';',encoding='iso-8859-1',  usecols=["POB_URBANA", "QRESIDUOS_DOM"]                       
                  )
-df
+data
 
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
 
-# Input widgets
 st.sidebar.subheader('Input features')
 sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.8)
 sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.1)
@@ -60,8 +59,6 @@ st.write(input_feature)
 
 # Print prediction output
 st.subheader('Output')
-st.metric('Predicted class', y_pred[0], '')
-
 
     st.caption(QUESTION_OR_FEEDBACK)
 
