453c3cffa5c14e0ae41b6b1ed672a9b0adb9272f
ronalth26 <117491977+ronalth26@users.noreply.github.com>
2023-07-10 16:19:20 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,8 @@ import altair as alt
 import numpy as np
 import seaborn as sns
 #from PIL import Image
-pip install seaborn 
+python -m pip install seaborn
+
 st.set_page_config(page_title="Datos Abiertos") # Nombre para configurar la pagina web
 st.header('Residuos municipalidades generados anualmente') #Va a ser el titulo de la pagina
 st.subheader('La GPC de residuos domiciliarios es un dato obtenido de los estudios de caracterización elaborados por las municipalidades provinciales y distritales y se refiere a la generación de residuos sólidos por persona-día.') #Subtitulo
