df91424a94d78345bb7a8d50c45a41520fe69d3d
ronalth26 <117491977+ronalth26@users.noreply.github.com>
2023-07-10 10:05:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,14 @@
 import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+st.set_page_config(page_title="Encuesta Oficial EPS") # Nombre para configurar la pagina web
+st.header('Resultados Encuestas Nacionales EPS Colombia 2022') #Va a ser el titulo de la pagina
+st.subheader('Cómo perciben los ciudadanos el servicio de las EPS en Colombia?') #Subtitulo
 
 nombre_variables = ['POB_URBANA', 'QRESIDUOS_DOM']
 
 excel_file  = pd.read_csv('https://raw.githubusercontent.com/ronalth26/git-prueba/master/lista-residuos.csv',sep=';',encoding='iso-8859-1'
                  )
 excel_file 
+
