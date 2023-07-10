b93790f69c9d1dbbc2248ad932e3dcb87f8193c6
ronalth26 <117491977+ronalth26@users.noreply.github.com>
2023-07-10 11:03:43 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import streamlit as st
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
 
 #from PIL import Image
 
@@ -12,3 +15,10 @@ nombre_variables = ["POB_URBANA", "QRESIDUOS_DOM"]
 df  = pd.read_csv('https://raw.githubusercontent.com/ronalth26/git-prueba/master/lista-residuos.csv',sep=';',encoding='iso-8859-1'
                  )
 df 
+
+
+st.subheader('El objetivo sería determinar si existe una relación lineal entre dos variables y predecir la generación de residuos domiciliarios para una población determinada.') #Subtitulo
+
+data  = pd.read_csv('https://raw.githubusercontent.com/ronalth26/git-prueba/master/lista-residuos.csv',sep=';',encoding='iso-8859-1',  usecols=["POB_URBANA", "QRESIDUOS_DOM"]                       
+                 )
+data
