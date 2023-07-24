1e2ca15216d4c1c53ca096e14931a6ffa3ec6b88
ronalth26 <117491977+ronalth26@users.noreply.github.com>
2023-07-24 16:41:46 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import altair as alt
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestClassifier
 
